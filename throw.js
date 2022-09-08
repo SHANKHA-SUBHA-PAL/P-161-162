6cb3ae4cadb86f86776550ec93f9d24a02cbfd5d
SHANKHA-SUBHA-PAL <subhankarpal76@gmail.com>
2022-09-08 20:44:13 +0530

    P-162 DONE

--- throw.js
+++ throw.js
@@ -8,7 +8,11 @@ AFRAME.registerComponent('bowling-ball', {
             if (e.key === "t") {
                 var ball = document.createElement("a-entity");
                 ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf")
-                ball.setAttribute("scale", { x: 3, y: 3, z: 3 })
+                ball.setAttribute("scale", {
+                    x: 3,
+                    y: 3,
+                    z: 3
+                })
 
                 var cam = document.querySelector("#camera")
                 pos = cam.getAttribute("position")
@@ -27,9 +31,43 @@ AFRAME.registerComponent('bowling-ball', {
                 ball.setAttribute("velocity", direction.multiplyScalar(-10))
 
                 var scene = document.querySelector("#arena")
+                //set the ball as dynamic body
+                ball.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: "10",
+                })
+                //add the collider event listener to the ball
+                ball.addEventListener("collide", this.removeBall)
+
                 scene.appendChild(ball)
             }
         })
-    }
+    },
+
+    removeBall: function (e) {
+        //ball element
+        var element = e.detail.body.el
+
+        //element which is hit
+        var elementHit = e.detail.body.el
+
+        if (elementHit.id.includes("pin")) {
+            //impulse & point vector
+            var impulse = new CANNON.Vec3(0, 1, -15)
+            var worldPoint = new CANNON.Vec3().copy(
+                elementHit.getAttribute("position")
+            )
+
+            elementHit.body.applyForce(impulse, worldPoint)
+
+            //remove event listener
+            element.removeEventListener("collide",this.removeBall)
+
+            //remove the balls from the scene
+            var scene = document.querySelector("#arena")
+            scene.removeChild(element)
+        }
+
+    },
 
 });
